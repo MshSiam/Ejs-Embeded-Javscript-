475829d4517a1458e9e7402d685443ad52faf3e4
MshSiam <siyamkenway@gmail.com>
2022-07-02 23:30:56 +0600

    ejs for loop

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express")
+const { copyFileSync } = require("fs")
 const app = express()
 const PORT = 3001
 
+app.use(express.urlencoded({ extended: true }))
+app.set("view engine", "ejs")
+
+let lan = []
+
 app.get("/", (req, res) => {
-  res.send("Hello")
+  res.render("index", { plNames: lan })
+})
+app.post("/", (req, res) => {
+  const lang = req.body.language
+  lan.push(lang.toUpperCase())
+  //   console.log(lang.toUpperCase())
+  res.redirect("/")
 })
 
 app.listen(PORT, () => {
